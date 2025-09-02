08a408805e2dae5845914399f2f72421067b318b
Gourita-M <mouad.gourita@gmail.com>
2025-09-02 13:36:42 +0100

    Added a code that uses if Statments to check is a number is zero negative or positive i had 1 mistak when giving the condition of num == to 0 . i typed num = 0 i got error and i fixed it

---
+++ Day 1 Conditions/Challenge 2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+        printf("Enter a Number to Check: ");
+            scanf("%d",&num);
+
+        if (num < 0)
+            printf("Your Number is Negative");
+
+            else if(num == 0)
+             printf("Your Number is Zero");
+
+               else
+                  printf("Your Number is Positive");
+        
+
+
+    return 0;
+}
+/*
+Challenge 6: Positive, Negative or Zero
+Write a program to check if the given number is positive, negative, or zero. 
+If the number is less than zero, then the number is negative, and if the number is greater than zero, then the number is positive. If both conditions are false, the number is zero.
+*/
