6da6a4157730f384783a87eb91d753c1ea772c85
Gourita-M <mouad.gourita@gmail.com>
2025-09-03 11:26:20 +0100

    Code to Find the biggest Number . i made one before with already fild in numbers but this time the user should enter the amount of numbers he wants to check . kinda tricky i had to use 2 loops the first one to get the user numbers and the second one to check which Number is the Biggest and it worked after fixing some other mistaks

---
+++ Day 2/Arrays/Challenge 4/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int ncount=0;
+    int numbers[ncount];
+    int max=0;
+
+    printf("Enter The Number of Elements To Find the Maximum: ");
+          scanf("%d",&ncount);
+
+      for (int i=0 ; i < ncount ; i++){
+             
+         printf("Enter The Number %d: ",i+1);
+          scanf("%d",&numbers[i]);
+    }
+    max = numbers[0];
+       for (int j=0 ; j < ncount ; j++){
+           if(numbers[j]> max){
+                max = numbers[j];
+           }  
+       }
+       printf("\n%d",max);
+    return 0;
+}
+/*
+Challenge 4: Find the Maximum
+Write a C program that finds and displays the largest element in an array of integers. 
+The program should ask for the number of elements and the elements in the array.
+*/
