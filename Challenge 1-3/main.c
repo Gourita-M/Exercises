160f72dcd7a4ff15b1cfa5f5f3e55a333537a9c8
Gourita-M <mouad.gourita@gmail.com>
2025-09-03 09:48:59 +0100

    code to store 5 valus and print them using arrays . the only problem that i used [1,2,3,4,5] instead of {} . but i fixed it

--- Challenge 1-3/main.c
+++ Challenge 1-3/main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 
 int main(){
+      float elect;
+      int usertype;
+      int contract;
+      float cost;
+      float bill;
+
+      printf("Enter Your Electricity Consumption: kwh");
+            scanf("%d",&elect);
+        
+      printf("User type (1 for residential, 2 for commercial): ");
+            scanf("%d",&usertype);
+
+      printf("Enter Your Contract Type (0 for standard, 1 for reduced): ");
+            scanf("%d",&contract);
+
+      printf("\n");
+
+        if (usertype == 1){
+            cost = elect * 0.20;
+                 if(contract == 0)
+                    cost = elect * 0.15 ;
+        } 
+        if (usertype == 2){
+            cost = elect * 0.30 ;
+                 if(contract == 1)
+                   cost = elect * 0.25
+
+        }
+
       
+    
+
+
+
+    if (elect > 500.0f) {
+        bill *= 1.10f;
+    }
+
+    // Output
+    printf("Electricity Bill: €%.2f\n", bill);
 
 
     return 0;
@@ -18,4 +57,7 @@ The rates are:
 Residential: €0.20/kWh for standard contract, €0.15/kWh for reduced contract
 Commercial: €0.30/kWh for standard contract, €0.25/kWh for reduced contract
 Add a 10% surcharge if consumption exceeds 500 kWh.
+// subtotal is the bill before surcharge
+if (consumption_kwh > 500) {
+    subtotal *= 1.10f
 */
---
+++ Day 2/Arrays/Challenge 1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+      int integers[]={1,2,3,4,5};
+
+      printf("%d",integers[0]);
+            printf("\n");
+      printf("%d",integers[1]);
+            printf("\n");
+      printf("%d",integers[2]);
+            printf("\n");
+      printf("%d",integers[3]);
+            printf("\n");
+      printf("%d",integers[4]);
+            printf("\n");
+
+    return 0;
+}
+/*
+Challenge 1: Initialization and Display
+Write a C program that initializes an array of integers with given values ​​and displays those values. 
+For example, you can initialize an array with the values [1, 2, 3, 4, 5]​​and display each value on a new line.
+*/
