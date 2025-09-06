c285882b84580dd7aff9753da64e56d54b9dc27f
Mouad <mouad.gourita@gmail.com>
2025-09-07 00:51:02 +0100

    Not That hard . i Tried to ask for full name but i shoud use fget but i;m not sure if i have to use it here

---
+++ Day 3/Strings/Challenge 1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+       char name[20];
+
+       printf("Enter Your Name: ");
+            scanf("%s",&name);
+
+       printf("Hey %s",name);
+
+    return 0;
+}
+/*
+Challenge 1: Displaying a Channel
+Write a C program that reads a string entered by the user and displays it on the screen.
+*/
