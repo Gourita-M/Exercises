1bb7f85bdb7889c6a6942f70e03e2745e1dcd1de
Mouad <mouad.gourita@gmail.com>
2025-09-07 11:23:30 +0100

    Wasn't hard but it took me time to find a way to do the code

--- Day 3/Strings/Challenge 2/main.c
+++ Day 3/Strings/Challenge 2/main.c
@@ -5,9 +5,9 @@ int main() {
     int length = 0;
 
     printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin); // Read string including spaces
+    fgets(str, sizeof(str), stdin);
 
-    // Count characters until null terminator
+
     while(str[length] != '\0' && str[length] != '\n') {
         length++;
     }
