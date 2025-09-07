b4f958a97574aa049d2f0a00bdae8a42747e9f14
Mouad <mouad.gourita@gmail.com>
2025-09-07 21:29:45 +0100

    Code To Convert the Capital Letters using toupper . i had to check on Strings libry to find it

---
+++ Day 3/Strings/Challenge 7/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>  
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    scanf(" %[^\n]", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        str[i] = toupper(str[i]);
+    }
+
+    printf("Uppercase string: %s\n", str);
+    return 0;
+}
+
+
+/*
+Challenge 7: Converting to Capital Letters
+Write a C program that reads a string and converts all characters to uppercase. Display the modified string.
+*/
