80603c6ae2070f41114f913dcb549e52359141bc
Mouad <mouad.gourita@gmail.com>
2025-09-07 01:03:27 +0100

    Not that hard but kinda triky to work on it without strlen

---
+++ Day 3/Strings/Challenge 2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); // Read string including spaces
+
+    // Count characters until null terminator
+    while(str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    printf("Length of the string: %d\n", length);
+    return 0;
+}
+
+/*
+Challenge 2: Chain Length
+Write a C program that calculates and displays the length of a string without using the function strlen().
+*/
